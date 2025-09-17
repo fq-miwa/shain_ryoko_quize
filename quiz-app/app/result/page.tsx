c66bf7bc316c188f498af0319ab7affeed1f2a31
fq-miwa <106648638+fq-miwa@users.noreply.github.com>
2025-09-18 00:42:45 +0900

    Update page.tsx

--- quiz-app/app/result/page.tsx
+++ quiz-app/app/result/page.tsx
@@ -2,9 +2,7 @@ export default function ResultPage() {
   return (
     <div>
       <h1 style={{ fontSize: 24, marginBottom: 8 }}>ご回答ありがとうございました！</h1>
-      <p>回答は匿名で保存されました。当日の集計にご協力ありがとうございます。</p>
+      <p>回答は匿名で保存されました。集計にご協力ありがとうございます。</p>
     </div>
   );
 }
-
-
