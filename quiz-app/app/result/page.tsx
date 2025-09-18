11a74188a2f6e64a0c952557e9dc1be29e06a1e4
fq-miwa <106648638+fq-miwa@users.noreply.github.com>
2025-09-18 23:11:09 +0900

    Update page.tsx

--- quiz-app/app/result/page.tsx
+++ quiz-app/app/result/page.tsx
@@ -1,7 +1,7 @@
 export default function ResultPage() {
   return (
     <div>
-      <h1 style={{ fontSize: 24, paddingTop: 50, paddingBottom: 50 }}>ご回答ありがとうございました！</h1>
+      <h1 style={{ fontSize: 24, paddingTop: 50 }}>ご回答ありがとうございました！</h1>
       <p style={{ margin: '16px 0', lineHeight: 1.8 }}>楽しい社員旅行にしましょう！</p>
       <div style={{ marginTop: 32, textAlign: 'center' }}>
         <img
@@ -13,3 +13,4 @@ export default function ResultPage() {
     </div>
   );
 }
+
